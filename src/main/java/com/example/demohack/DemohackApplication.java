36346ea336f69cb77688888c83dd18d6f1c07c1c
ShonSagoro <ramosproque@gmail.com>
2022-12-01 00:58:51 -0600

    first commit

---
+++ src/main/java/com/example/demohack/DemohackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demohack/DemohackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
