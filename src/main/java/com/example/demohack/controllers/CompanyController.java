6b42b1d29b24020019db9e5a79982a1b30586664
MrMendez <hirammendez000@gmail.com>
2022-12-01 01:58:20 -0600

    company controller added

--- src/main/java/com/example/demohack/controllers/CompanyController.java
+++ src/main/java/com/example/demohack/controllers/CompanyController.java
@@ -1,4 +1,49 @@
 package com.example.demohack.controllers;
 
+import com.example.demohack.controllers.dtos.request.CreateCompanyRequest;
+import com.example.demohack.controllers.dtos.request.UpdateCompanyRequest;
+import com.example.demohack.controllers.dtos.response.BaseResponse;
+import com.example.demohack.services.interfaces.ICompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("company")
 public class CompanyController {
+    @Autowired
+    private ICompanyService service;
+
+    @GetMapping("{id}")
+    ResponseEntity<BaseResponse> get (@PathVariable Long id) {
+        BaseResponse baseResponse = service.get(id);
+        return new ResponseEntity<>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @GetMapping
+    ResponseEntity<BaseResponse> getAll ( ) {
+        BaseResponse baseResponse = service.getAll();
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @PostMapping
+    ResponseEntity<BaseResponse> create (@RequestBody @Valid CreateCompanyRequest request) {
+        BaseResponse baseResponse = service.create(request);
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @PutMapping("{id}")
+    ResponseEntity<BaseResponse> update (@PathVariable @Valid Long id, @RequestBody UpdateCompanyRequest request) {
+        BaseResponse baseResponse = service.update(id, request);
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @DeleteMapping("{id}")
+    void delete (@PathVariable Long id) {
+        service.delete(id);
+    }
+
 }
--- src/main/java/com/example/demohack/entities/Company.java
+++ src/main/java/com/example/demohack/entities/Company.java
@@ -1,15 +1,18 @@
 package com.example.demohack.entities;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
 public class Company {
     @Id
-    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
