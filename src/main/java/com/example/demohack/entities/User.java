a88445e784311a099d07231a301e925848ab123d
MrMendez <hirammendez000@gmail.com>
2022-12-01 02:47:57 -0600

    entitie and repository added

---
+++ src/main/java/com/example/demohack/entities/User.java
@@ -0,0 +1,20 @@
+package com.example.demohack.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class User {
+    @Id
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
