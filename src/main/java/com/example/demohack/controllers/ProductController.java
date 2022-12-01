6bad206f5a667519f8c0da3880f84367e78659ed
MrMendez <hirammendez000@gmail.com>
2022-12-01 04:26:35 -0600

    controller added

---
+++ src/main/java/com/example/demohack/controllers/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.demohack.controllers;
+
+import com.example.demohack.controllers.dtos.request.CreateProductRequest;
+import com.example.demohack.controllers.dtos.request.CreateUserRequest;
+import com.example.demohack.controllers.dtos.request.UpdateProductRequest;
+import com.example.demohack.controllers.dtos.request.UpdateUserRequest;
+import com.example.demohack.controllers.dtos.response.BaseResponse;
+import com.example.demohack.services.interfaces.IProductService;
+import com.example.demohack.services.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+    @Autowired
+    private IProductService service;
+
+    @GetMapping("{id}")
+    ResponseEntity<BaseResponse> get (@PathVariable Long id) {
+        BaseResponse baseResponse = service.get(id);
+        return new ResponseEntity<>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @GetMapping
+    ResponseEntity<BaseResponse> getAll ( ) {
+        BaseResponse baseResponse = service.getAll();
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @PostMapping
+    ResponseEntity<BaseResponse> create (@RequestBody @Valid CreateProductRequest request) {
+        BaseResponse baseResponse = service.create(request);
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @PutMapping("{id}")
+    ResponseEntity<BaseResponse> update (@PathVariable @Valid Long id, @RequestBody UpdateProductRequest request) {
+        BaseResponse baseResponse = service.update(id, request);
+        return new ResponseEntity<BaseResponse>(baseResponse, baseResponse.getHttpStatus());
+    }
+
+    @DeleteMapping("{id}")
+    void delete (@PathVariable Long id) {
+        service.delete(id);
+    }
+
+}
