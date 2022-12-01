834691903bfc38558be1ec2c0bbeea3363061654
ShonSagoro <ramosproque@gmail.com>
2022-12-01 07:58:57 -0600

    cors updated

--- src/main/java/com/example/demohack/DemohackApplication.java
+++ src/main/java/com/example/demohack/DemohackApplication.java
@@ -2,12 +2,28 @@ package com.example.demohack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemohackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemohackApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD")
+						.allowedHeaders("*");
+				}	
+		};
+		
 	}
 
 }
